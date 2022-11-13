eaea645f1531f8fbf82d090b86a0ed00cda18e9d
tamlv <tamle.dev@gmail.com>
2022-11-13 09:13:18 +0700

    add header & footer

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="Home">
+    <Header/>
     <h1>
       This is a home page
     </h1>
   </div>
+  <Footer/>
 </template>
+
+<script>
+import Header from '../components/Header/Header.vue'
+import Footer from '../components/Footer/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Footer
+  }
+}
+
+</script>
