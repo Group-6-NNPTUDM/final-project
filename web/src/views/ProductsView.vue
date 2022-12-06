17a8e3de7d898e5698e29c3c433d7097e2ab707e
tamlv <tamle.dev@gmail.com>
2022-12-06 12:32:30 +0700

    updated code

--- web/src/views/ProductsView.vue
+++ web/src/views/ProductsView.vue
@@ -1,10 +1,20 @@
 <script>
 import HeaderVue from "@/components/Header/Header.vue";
 import FooterVue from "@/components/Footer/Footer.vue";
-import axios from "axios";
 import CommonConstant from "@/constants/commonConstant";
+import { getProducts, buildUrlProductById } from "@/services/productsService";
+import {
+  APPLE_DESC,
+  DELL_DESC,
+  THINKPAD_DESC,
+} from "@/constants/descriptionConstant";
 
 export default {
+  created() {
+    (this.APPLE_DESC = APPLE_DESC),
+      (this.DELL_DESC = DELL_DESC),
+      (this.THINKPAD_DESC = THINKPAD_DESC);
+  },
   data() {
     return {
       products: [],
@@ -15,6 +25,7 @@ export default {
       sortByDefault: CommonConstant.SORT_BY_TITLE,
       orderByDefault: CommonConstant.ORDER_ASC,
       modelYearsChecked: [],
+      categoryType: null,
       dummyModelYear: [
         {
           id: 0,
@@ -31,6 +42,7 @@ export default {
   },
   async mounted() {
     const categoryType = this.$route.params.category;
+    this.categoryType = categoryType;
     const response = await getProducts(
       this.defaultPage,
       this.limitItems,
@@ -40,9 +52,10 @@ export default {
     this.type = categoryType;
     this.products = response.products;
     this.totalCount = response.totalCount;
-    this.numberOfPaginateBtn = Math.ceil(response.totalCount / this.limitItems);
+    this.updateNumberOfPaginate(response.totalCount, this.limitItems);
   },
   methods: {
+    buildUrlProductById: buildUrlProductById,
     async handlePaginationBtnClick(pageIndex) {
       const res = await getProducts(
         pageIndex,
@@ -65,10 +78,9 @@ export default {
       this.sortByDefault = sortType;
       this.orderByDefault = orderType;
       this.products = res.products;
-      this.numberOfPaginateBtn = Math.ceil(res.totalCount / this.limitItems);
+      this.updateNumberOfPaginate(res.totalCount, this.limitItems);
     },
     async handleFilterByModelYear(object) {
-      // find modelyear
       const modelYear = this.dummyModelYear.find((obj) => obj.id === object.id);
 
       modelYear.checked = !modelYear.checked;
@@ -81,8 +93,6 @@ export default {
         );
 
         this.modelYearsChecked.shift(indexOfModelYear, 1);
-
-        console.log(this.modelYearsChecked, "test");
       }
 
       const yearFiltered = this.modelYearsChecked.map((year) => year.year);
@@ -97,49 +107,16 @@ export default {
       );
 
       this.products = res.products;
-      this.numberOfPaginateBtn = Math.ceil(res.totalCount / this.limitItems);
+      this.updateNumberOfPaginate(res.totalCount, this.limitItems);
     },
+    updateNumberOfPaginate(totalCount, limitItems) {
+      this.numberOfPaginateBtn = Math.ceil(totalCount / limitItems)
+    }
   },
   components: {
     HeaderVue,
     FooterVue,
   },
-};
-
-const getProducts = async (
-  page,
-  limit = 1,
-  type = this.$route.category,
-  sortType = CommonConstant.SORT_BY_TITLE,
-  orderType = CommonConstant.ORDER_ASC,
-  modelYear = []
-) => {
-  const emptyLength = 0;
-  const templateModelYear = `&modelYear=`;
-  let totalCountUrl = `http://localhost:8000/api/products?categories=${type}`;
-  let productsUrl = `http://localhost:8000/api/products?_page=${page}&_limit=${limit}&_sort=${sortType}&_order=${orderType}&categories=${type}`;
-
-  if (modelYear.length !== emptyLength) {
-    modelYear.forEach((year) => {
-      const templateModelYearFormatted = templateModelYear + `${year}`;
-      totalCountUrl = `${totalCountUrl}${templateModelYearFormatted}`;
-      productsUrl = `${productsUrl}${templateModelYearFormatted}`;
-    });
-  }
-  const totalCount = await axios
-    .get(totalCountUrl)
-    .then((res) => res.data.length)
-    .catch((err) => console.log(err));
-
-  const products = await axios
-    .get(productsUrl)
-    .then((res) => res.data)
-    .catch((err) => console.log(err));
-
-  return {
-    products: products,
-    totalCount: totalCount,
-  };
 };
 </script>
 
@@ -150,57 +127,54 @@ const getProducts = async (
       <div class="row">
         <div class="col-2">
           <div class="categories-sort">
-            <p>Sắp xếp</p>
-            <button @click="() => handleSortBtnClick('title', 'asc')">
-              Sếp theo tên : A -> Z</button
-            ><br />
-            <button @click="() => handleSortBtnClick('title', 'desc')">
-              Sếp theo tên : Z -> A</button
-            ><br />
-            <button @click="() => handleSortBtnClick('price', 'asc')">
-              Sếp theo giá : Thấp -> Cao</button
-            ><br />
-            <button @click="() => handleSortBtnClick('price', 'desc')">
-              Sếp theo tên : Cao -> Thấp</button
-            ><br />
+            <h6 class="fw-bold">Sắp xếp</h6>
+            <button class="btn btn-primary btn-sm" @click="() => handleSortBtnClick('title', 'asc')">
+              Sếp theo tên : A -> Z</button><br />
+            <button class="btn btn-primary btn-sm mt-2" @click="() => handleSortBtnClick('title', 'desc')">
+              Sếp theo tên : Z -> A</button><br />
+            <button class="btn btn-primary btn-sm mt-2" @click="() => handleSortBtnClick('price', 'asc')">
+              Sếp theo giá : Thấp -> Cao</button><br />
+            <button class="btn btn-primary btn-sm mt-2" @click="() => handleSortBtnClick('price', 'desc')">
+              Sếp theo tên : Cao -> Thấp</button><br />
           </div>
-          <div class="productsfilter_modalyear mt-5">
-            <h6>Model Year</h6>
-            <div
-              class="form-check"
-              v-for="modelYear in dummyModelYear"
-              v-bind:key="modelYear.id"
-            >
-              <input
-                class="form-check-input"
-                type="checkbox"
-                value=""
-                id="modelYear"
-                @click="() => handleFilterByModelYear(modelYear)"
-              />
-              <label class="form-check-label" for="flexCheckDefault">
+          <div class="productsfilter_modalyear mt-2">
+            <h6 class="fw-bold">Model Year</h6>
+            <div class="form-check" v-for="modelYear in dummyModelYear" v-bind:key="modelYear.id">
+              <input class="form-check-input" type="checkbox" value="" id="modelYear" @click="() => handleFilterByModelYear(modelYear)" />
+              <label class="form-check-label fw-bold" for="flexCheckDefault">
                 {{ modelYear.year }}
               </label>
             </div>
           </div>
         </div>
         <div class="col-10">
-          <div class="row category-title">Mac Store</div>
+          <h4 class="row category-title fw-bold">Mac Store</h4>
+          <div v-show="categoryType === 'apple'">
+            <h6 class="text-center fw-bold">{{ APPLE_DESC.title }}</h6>
+            <p>{{ APPLE_DESC.desc }}</p>
+          </div>
+          <div v-show="categoryType === 'lenovo'">
+            <h6 class="text-center fw-bold">{{ THINKPAD_DESC.title }}</h6>
+            <p>{{ THINKPAD_DESC.desc }}</p>
+          </div>
+          <div v-show="categoryType === 'dell'">
+            <h6 class="text-center fw-bold">{{ DELL_DESC.title }}</h6>
+            <p>{{ DELL_DESC.desc }}</p>
+          </div>
           <div class="row category-description">
             <div v-for="product in products" :key="product.id" class="col-4">
               <img :src="product.img" alt="" />
-              <p>{{ product.title }}</p>
+              <p>
+                {{ product.title }}
+                <a v-bind:href="buildUrlProductById(product.id)">see more</a>
+              </p>
               <p>{{ product.price }}</p>
             </div>
           </div>
           <!-- Phân trang  -->
           <nav aria-label="Page navigation example">
             <ul class="pagination" v-if="numberOfPaginateBtn > 1">
-              <li
-                class="page-item"
-                v-for="(btn, index) in numberOfPaginateBtn"
-                :key="index"
-              >
+              <li class="page-item" v-for="(btn, index) in numberOfPaginateBtn" :key="index">
                 <button @click="() => handlePaginationBtnClick(index + 1)">
                   {{ btn }}
                 </button>
