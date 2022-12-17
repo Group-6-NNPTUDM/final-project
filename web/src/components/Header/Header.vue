9e4f2b13b704f37f2b562d0e0288c42a2b741295
Tam2412 <tamle.dev@gmail.com>
2022-12-18 00:54:44 +0700

    add search page

--- web/src/components/Header/Header.vue
+++ web/src/components/Header/Header.vue
@@ -2,7 +2,19 @@
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      keyword: "",
+    };
+  },
   components: {},
+  methods: {
+    searchKeyword() {
+      console.log(this.keyword, "keyword");
+
+      this.$router.push(`/tim-kiem/${this.keyword}`);
+    },
+  },
 };
 </script>
 
@@ -22,28 +34,47 @@ export default {
       <div class="col-2 top-logo">
         <div class="logo-container">
           <a href="http://localhost:8080/">
-            <img src="https://mac24h.vn/images/logos/42/logo-mac24h_zwyz-ad.png?t=1629384503" width="145px" height="46" alt="logo" />
+            <img
+              src="https://mac24h.vn/images/logos/42/logo-mac24h_zwyz-ad.png?t=1629384503"
+              width="145px"
+              height="46"
+              alt="logo"
+            />
           </a>
         </div>
       </div>
       <div class="col-6">
         <div class="row">
-          <ul class="
+          <ul
+            class="
               nav
               col-12 col-lg-auto
               me-lg-auto
               mb-2
               justify-content-center
               mb-md-0
-            ">
+            "
+          >
             <li class="nav-item active">
-              <a href="http://localhost:8080/danh-muc-san-pham/apple" class="nav-link px-2 text-white">APPLE</a>
+              <a
+                href="http://localhost:8080/danh-muc-san-pham/apple"
+                class="nav-link px-2 text-white"
+                >APPLE</a
+              >
             </li>
             <li class="nav-item dropdown">
-              <a href="http://localhost:8080/danh-muc-san-pham/lenovo" class="nav-link text-white">LENOVO - THINKPAD</a>
+              <a
+                href="http://localhost:8080/danh-muc-san-pham/lenovo"
+                class="nav-link text-white"
+                >LENOVO - THINKPAD</a
+              >
             </li>
             <li class="nav-item active">
-              <a href="http://localhost:8080/danh-muc-san-pham/dell" class="nav-link text-white">DELL - XPS</a>
+              <a
+                href="http://localhost:8080/danh-muc-san-pham/dell"
+                class="nav-link text-white"
+                >DELL - XPS</a
+              >
             </li>
             <li>
               <a href="#" class="nav-link text-white">Hỗ Trợ</a>
@@ -52,7 +83,8 @@ export default {
         </div>
       </div>
       <div class="col-4">
-        <form class="
+        <form
+          class="
             form-inline
             d-flex
             justify-content-center
@@ -60,8 +92,16 @@ export default {
             form-sm
             active-cyan-2
             mt-2
-          ">
-          <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+          "
+        >
+          <input
+            class="form-control form-control-sm mr-3 w-75"
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+            v-model="keyword"
+            v-on:keydown.enter.prevent="searchKeyword()"
+          />
           <i class="fas fa-search" aria-hidden="true"></i>
         </form>
       </div>
---
+++ web/src/views/SearchView.vue
@@ -0,0 +1,35 @@
+<script>
+import Header from "../components/Header/Header.vue";
+import Footer from "../components/Footer/Footer.vue";
+import axios from "axios";
+
+export default {
+  setup() {},
+  mounted() {
+    const keyword = this.$route.params.keyword;
+    this.getProductByKeyword(keyword);
+  },
+  data() {
+    return {
+      keyword: "",
+    };
+  },
+  methods: {
+    getProductByKeyword(keyword) {
+      console.log(keyword, "params nè");
+    },
+  },
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Header />
+    <div class="container bg-danger mt-5">1</div>
+    <Footer />
+  </div>
+</template>
