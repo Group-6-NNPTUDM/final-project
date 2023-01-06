8868c9dbfa3260ea4674a76d403685e316efb2ef
ZombieNDD <nguyen.d.dung00@gmail.com>
2023-01-06 15:50:54 +0700

    Update

--- web/src/views/PayView.vue
+++ web/src/views/PayView.vue
@@ -57,34 +57,6 @@
             <th>Số lượng</th>
             <th>Tổng tiền</th>
           </tr>
-          <tr v-for="product in ProductsCart" :key="(product.id)">
-            <td>{{ product.title }}</td>
-            <td>{{ product.count }}</td>
-            <td>{{ result(product.count, product.price) }}</td>
-          </tr>
-        </table>
-        <div class="form-group">
-          <div class="row">
-            <div class="my-a-infor">Email:</div>
-            <input class="input-text" type="text" style="margin-bottom: 20px" />
-          </div>
-        </div>
-
-        <div class="form-group">
-          <div class="row">
-            <input style="width: 45%; margin: auto" type="submit" value="Thanh toán" class="btn btn-outline-success" />
-            <a class="btn btn-outline-danger" style="width: 45%; margin: auto" href="/gio-hang/">Quay lại</a>
-          </div>
-        </div>
-      </div>
-      <div class="my-swapper col-sm-4">
-        <h3>HOÁ ĐƠN</h3>
-        <table>
-          <tr>
-            <th>Tên sản phẩm</th>
-            <th>Số lượng</th>
-            <th>Tổng tiền</th>
-          </tr>
 
           <tr v-for="product in ProductsCart" :key="product.id">
             <td>{{ product.title }}</td>
