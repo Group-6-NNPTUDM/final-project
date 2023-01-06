6e7171ac72d70eb9b3fd2c465f80c9eb00cdc7c7
Tam2412 <tamle.dev@gmail.com>
2023-01-06 23:28:34 +0700

    fixed bug

--- web/src/components/ProductDetail/ProductDetail.vue
+++ web/src/components/ProductDetail/ProductDetail.vue
@@ -1,32 +1,38 @@
 <template>
   <div class="card-group">
     <div class="card" v-for="item in data" v-bind:key="item.index">
-      <img class="card-img-top" v-bind:src="item.img" alt="Card image cap">
-      div class="card-body productDetail">
-      <h6 class="card-title">{{item.title}}
-        <a v-bind:href="`http://localhost:8080/san-pham/${item.id}`">see more</a>
-      </h6>
-      <p class="card-text"><small class="text-muted">{{ item.price.toLocaleString('it-IT', {
-            style: 'currency',
-            currency: 'VND'
-          })
-        }}</small></p>
+      <img class="card-img-top" v-bind:src="item.img" alt="Card image cap" />
+      <div class="card-body productDetail">
+        <h6 class="card-title">
+          {{ item.title }}
+          <a v-bind:href="`http://localhost:8080/san-pham/${item.id}`"
+            >see more</a
+          >
+        </h6>
+        <p class="card-text">
+          <small class="text-muted">{{
+            item.price.toLocaleString("it-IT", {
+              style: "currency",
+              currency: "VND",
+            })
+          }}</small>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import "@/assets/css/product/style.css"
+import "@/assets/css/product/style.css";
 export default {
   props: {
-    data: []
+    data: [],
   },
-  name: 'ProductDetail',
+  name: "ProductDetail",
   mounted() {
-    console.log(this.data, 'data');
-  }
-}
-
+    console.log(this.data, "data");
+  },
+};
 </script>
 
 <style scoped>
